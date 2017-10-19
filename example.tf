3d3efdf607427e7ef3a0985028a2e3214486e3e6
iotg-matt-pease <matt.pease@iotecglobal.com>
2017-10-19 09:35:13 +0100

    Added output of instance ip

--- example.tf
+++ example.tf
@@ -3,8 +3,14 @@ provider "aws" {}
 resource "aws_instance" "example" {
   ami           = "ami-85d30efc"
   instance_type = "t1.micro"
+  key_name = "ansible"
+  vpc_security_group_ids = ["sg-8ee20cf5"]
 }
 
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.ip.public_ip}"
 }
